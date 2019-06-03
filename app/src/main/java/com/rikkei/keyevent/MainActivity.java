4d161f5294a93a9f4be5ee48d620d1314a5ddaf1
vuquoctrong <vuquoctrong141@gmail.com>
2019-06-03 15:44:36 +0700

    butterknife

--- app/src/main/java/com/rikkei/keyevent/MainActivity.java
+++ app/src/main/java/com/rikkei/keyevent/MainActivity.java
@@ -2,23 +2,44 @@ package com.rikkei.keyevent;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.btn0) Button btn0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        btn0.setOnClickListener(this);
+
     }
 
 
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn0:
+                Toast.makeText(this, "số 0", Toast.LENGTH_LONG).show();
+                break;
+            default:
+                break;
+        }
+
+    }
 }
