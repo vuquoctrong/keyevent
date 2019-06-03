8a9cb2c6fc5a27f6cd8ae7f04004cc8b034c22e5
vuquoctrong <vuquoctrong141@gmail.com>
2019-06-03 14:56:59 +0700

    activity ban phim

--- app/src/main/java/com/rikkei/keyevent/MainActivity.java
+++ app/src/main/java/com/rikkei/keyevent/MainActivity.java
@@ -11,61 +11,14 @@ import android.widget.TimePicker;
 
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
-    private Button btnDate;
-    private Button btnTime;
+public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
     }
 
-    private void init() {
-        btnDate = findViewById(R.id.btnDialogDay);
-        btnTime = findViewById(R.id.btnDialogTime);
-        btnDate.setOnClickListener(this);
-        btnTime.setOnClickListener(this);
-    }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnDialogDay:
-                showDatePickerDiaLog();
-                break;
-            case R.id.btnDialogTime:
-                showTimePickerDiaLog();
-                break;
-            default:
-                break;
-        }
-    }
-    private void showTimePickerDiaLog() {
-        Calendar calendar = Calendar.getInstance();
-        final int hour = calendar.get(Calendar.HOUR_OF_DAY);
-        int minute = calendar.get(Calendar.MINUTE);
-        TimePickerDialog timePickerDialog =
-                new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                    }
-                }, hour, minute, true);
-
-        timePickerDialog.show();
-    }
-    private void showDatePickerDiaLog(){
-        Calendar calendar = Calendar.getInstance();
-        int year = calendar.get(Calendar.YEAR);
-        int month = calendar.get(Calendar.MONTH);
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
-        DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-
-            }
-        }, year, month, day);
-        datePickerDialog.show();
-    }
 }
